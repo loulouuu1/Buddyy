3d229d83e0409955f3b5e80c148d8585f34febc6
loulouuu1 <133874984+loulouuu1@users.noreply.github.com>
2025-05-09 10:33:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,8 +7,69 @@ Nianiaaaa je sais que tu traverses pas la période la plus facile et vraiment ju
 This is my small token of appreciation for u, luv u`,
 
   `To be honest, you completely shook off my life & my routines.
-Never ever would I have expected that i would spend time to code something, knowing how much I hate coding. Yet, no matter how much I despise it, whenever it's uuuu who talks about ur work, ca switch instant et mon cerveau classe direct ca dans la categorie " interessant, à save " `,
+Never ever would I have expected that i would spend time to code something, knowing how much I hate coding. 
+Yet, no matter how much I despise it, whenever it's uuuu who talks about ur work, ca switch instant et mon cerveau classe direct ca dans la categorie " interessant, à save " 
+Tu t'en rends pas compte mais t'as vraiment le pouvoir de changer les gens et c'est cet espèce de feeling qu est le plus attrayant chez toiii, genre 1er degré, on a l'impression de decouvrir de nouvelles emotions, de vouloir s'ouvrir et tout, je jure en amie normale u suck u re the worst mdrrr mais en buddy u incrrrrrrrrr.  `,
 
+  `Celle là, elle est un peu plus personnelllle, tsais que mes premières soirées enjoyables, c'était avec toiii? 
+Mes premières cuites, mes premiers vomis, mes premiers champis, ma 1ère pompe car ui je m'étais déja fait pompé mdrrr mais jamais eu de pompe de jaeger !!!`
+
+  `Y a moyen de Clotairerrrr ouuuu? Eh on se rend pas compte mais 100 mots c durrr jss habitué à te faire des pavés mais po comme ca oops so mmmh let me startttt
+  J'eus aimé use ChatGPT mais ca aurait po été réel so nahhhh
+  1. 빛 
+  2. Bitch (a different one)
+  3. Cute
+  4. Luvelyyyyyyyy
+  5. Alive je jure dans les temps qui courent, c'est un compliment sa race
+  6. Funnyyyy 
+  7. Understanding
+  8. Bright on a daily basis
+  9. Shiny like the stars after a cabane party 
+  10. Honestttt
+  11. Weird but the world is weird so heheeee
+  12. Pouty so we match heheheee 
+  13. Chatouillleuse so c drole everytimmeeee
+  14. Moodyyy ur life is a mess, u re always a mood but that's what makes u who u areeeee
+  15. Uniqueeee (je jure je supporterais po 2 shiwwu mdrrr but 1 is perfectttt)
+  16. MY buddy, ca je jure c le best compliment and i mean it 💛
+  17. Relatableeeee 
+  18. Rly caringg about everyone, even strangerssss
+  19. Rly oPeN mInDeD ~ Hongdae Girl sur nous 
+  20. U have Lune Personality cccc
+  21. U just a girlll 🎀
+  22. Karma Mamaaaa 
+  23. Nous avons le maitre du jeu et with u le maitre des potinssss ooops tu sais tjrs tt sur tt c est tro biennnn
+  24. Smarttttt mm si tu l'assumes poooo, rly wouldnt spend this much time with a dumb person but never ever have i considered u to be dumb or of a lower IQ cause u arennnnnttttt 
+  25. Dummmyyy (tkt juste apres le smart c illegal mdrrrr)
+  26. Sensibleeee mais c ce qui fait ton "charme" bitch t pas charmante ta race mais charme on se comprend genre, ca donne envie de "take care of u" et javoue j adore persooo
+  27. "Sensible" but Sarcastic :) :) :) :) :) :) 
+  28. BPD Girly so we never know which emotion will hit us today !!!! 
+  29. Extraaaa as u should ~ 
+  30. A Literal Queen 👑
+  31. U slayyyyyyyyyyyyy 
+  32. U da best oops
+  33. T pleine de defauuuuts, ce qui te rend  tellement plus humaine, relatable, accessible et surement une des raisons pr lesquelles j t'adore autant
+  34. Our luv languages are kinda opposite yet u always try to reach out to me to make sure both of us are happy et ca i will forever be grateful, que ce soit les vocs, play ensemble, donner des nouvelles sans raisons, ttttt vrmnt merchiiii 
+  35. Lou-ishhhhh which means u kinda act like a Lou sauvage sometimes et hihiiiiii
+  36. Ur tastess are Peak Tier 👑💎👑💎👑💎👑💎👑💎👑💎👑💎👑💎👑💎👑💎
+  37. On aime les mm choses so c'est perffffffff 
+  38. U adorableeee when u try to watch/listen to/read something i like alors que de base c'est po ur cup of tea (car ui nous sommes dans Bridgerton mdr)
+  39. Hors periodes délicates, on se sent tjrs à l'aiseeee en te parlant 
+  40. Having u is soothing mmmh idk how to explain but like having someone i can tell everything everytime bah ca apaise sa race 
+  41. u're Smoooooollllll
+  42. Ur eyes are flawless, mmmh si jdevais les decrireeee j dirais qu on est direct attiré que par tes yeuuux (après j  avoue jss une merde mais jregarde po bcp le reste de ton visage je) pcq soit ils sont hyper profonds  soit c est l inverse et la clarté est apaisante je
+  43. A binge readerrrrrr mais je jure j ai trop type jvais repartir aux mots qui motent je
+  44. Selflesss cause even when ppl hurt u, ca prend du temps mais tu te mets à leur place et u stay 
+  45. Rly Big Heart ... if i may u luv/lov/love too much et ur heart shatters everytime et it hurts to see u hurt :(
+  46. Savage ~ #ripToutesLesPersonnesQuiSeSontFaitInsulterParToiAcauseDeMoi oooppps
+  47. Sassy girllll je jure jss impertinent de baisé but u worseee mdrrrr jadoooore
+  48. My Masterrrr ccccc
+  49. D R A M A 👒 je jure si tt dans expedition 33 t'aurais fini par etre la buddy de la Peintresse mdrrrr
+  50. Luved by lou ~ could keep keeepppp but its just a first version of my site for youuu, cant do the 100 if it's not as perfect as u so t'auras les 100 qd jserai asssz boooon
+`
+  
+  `Tout ça pour dire que we shared sooo many moments together, et pourtant  ca fait que commmencerrrr :p 
+Wont lose u agaiiiin and dun wowwy we will make our trips to Korea, Japan & China (fais ton passeport chacale nn?) heheeeeee`
   "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""
 ];
 
